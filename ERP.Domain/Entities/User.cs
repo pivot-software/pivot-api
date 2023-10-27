e42adcdea13211b088c90e342f3c759d787bf9c7
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-10-27 10:46:03 -0300

    feat: create entity of user and migration

--- ERP.Domain/Entities/User.cs
+++ ERP.Domain/Entities/User.cs
@@ -1,14 +1,38 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ERP.Domain.Entities;
-
-public class User
+namespace ERP.Domain.Entities
 {
-    public Guid UserId { get; set; } = Guid.NewGuid();
+    [Table("users")]
+    public class User
+    {
+        [Column("id")]
+        public Guid UserId { get; set; } = Guid.NewGuid();
 
-    public string Email { get; set; } = null!;
+        [Column("email")]
+        public string Email { get; set; } = null!;
 
-    public string Password { get; set; } = null!;
+        [Column("username")]
+        public string Username { get; set; } = null!;
 
-    public string Role { get; set; } = null!;
+        [Column("password")]
+        public string Password { get; set; } = null!;
+
+        [Column("avatar")]
+        public string Avatar { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Column("invited_by")]
+        public Guid? InvitedBy { get; set; }
+
+        [Column("deleted_by")]
+        public Guid? DeletedBy { get; set; }
+
+        [Column("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+
+
+    }
 }
