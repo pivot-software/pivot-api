1f152d0d5909c48f993c518f5e86b1a1e24c6ad0
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-11-01 08:30:00 -0300

    feat: create migration of profiles and permissions

---
+++ src/ERP.Api/Migrations/20231101112548_UpdateTables.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ERP.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_workspaces_users_admin_id",
+                table: "workspaces");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "admin_id",
+                table: "workspaces",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddColumn<int>(
+                name: "profile_id",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "permissions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    resource = table.Column<string>(type: "text", nullable: false),
+                    action_type = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    deleted_by = table.Column<Guid>(type: "uuid", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_permissions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "profile",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    deleted_by = table.Column<Guid>(type: "uuid", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_profile", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_profile_id",
+                table: "users",
+                column: "profile_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_profile_profile_id",
+                table: "users",
+                column: "profile_id",
+                principalTable: "profile",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_workspaces_users_admin_id",
+                table: "workspaces",
+                column: "admin_id",
+                principalTable: "users",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_profile_profile_id",
+                table: "users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_workspaces_users_admin_id",
+                table: "workspaces");
+
+            migrationBuilder.DropTable(
+                name: "permissions");
+
+            migrationBuilder.DropTable(
+                name: "profile");
+
+            migrationBuilder.DropIndex(
+                name: "IX_users_profile_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "profile_id",
+                table: "users");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "admin_id",
+                table: "workspaces",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_workspaces_users_admin_id",
+                table: "workspaces",
+                column: "admin_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
